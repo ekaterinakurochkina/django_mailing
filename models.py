09aaac157b5100e718e44491f785ac500f20df26
Katerina Kurochkina <katifedr@mail.ru>
2025-01-11 18:13:05 +0500

    модели

--- models.py
+++ models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True, help_text="Введите номер телефона")
+    avatar = models.ImageField(upload_to="users/avatars", verbose_name='Аватар', blank=True, null=True, help_text='Загрузите свой аватар')
+
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['email']
